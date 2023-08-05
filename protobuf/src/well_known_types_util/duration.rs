9c448568410e2e404db5099366d2628f69325d9b
Matt Klein <mklein@bitdrift.io>
2023-08-05 10:56:25 -0600

    fix proto duration Into implementation
    This can fail, and should implement TryInto.
    
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- protobuf/src/well_known_types_util/duration.rs
+++ protobuf/src/well_known_types_util/duration.rs
@@ -28,15 +28,18 @@ impl From<std::time::Duration> for Duration {
 /// Convert to `std::time::Duration`.
 ///
 /// This conversion might be lossy if `std::time::Duration` precision is smaller than nanoseconds.
-///
-/// # Panics
-///
-/// If `Duration` value is outside of `std::time::Duration` supported range.
-impl Into<std::time::Duration> for Duration {
-    fn into(self) -> std::time::Duration {
-        assert!(self.seconds >= 0);
-        std::time::Duration::from_secs(self.seconds as u64)
-            + std::time::Duration::from_nanos(self.nanos as u64)
+impl TryInto<std::time::Duration> for Duration {
+    type Error = &'static str;
+
+    fn try_into(self) -> std::result::Result<std::time::Duration, Self::Error> {
+        // I'm not sure what it means to have positive seconds and negative nanos so just fail if
+        // either are < 0.
+        if self.seconds < 0 || self.nanos < 0 {
+            return Err("proto duration < 0");
+        }
+
+        Ok(std::time::Duration::from_secs(self.seconds as u64)
+            + std::time::Duration::from_nanos(self.nanos as u64))
     }
 }
 
@@ -50,7 +53,7 @@ mod test {
             assert_eq!(duration, Duration::from(std_time_duration));
             assert_eq!(
                 std_time_duration,
-                Into::<std::time::Duration>::into(duration)
+                TryInto::<std::time::Duration>::try_into(duration).unwrap()
             );
         }
 
@@ -63,5 +66,12 @@ mod test {
             },
             std::time::Duration::from_millis(4_123),
         );
+
+        TryInto::<std::time::Duration>::try_into(Duration {
+            seconds: -1,
+            nanos: 0,
+            ..Default::default()
+        })
+        .unwrap_err();
     }
 }
